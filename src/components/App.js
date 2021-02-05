2539465f78356b29a9f8e6f39d7c11a6a4c0109b
Ori Markowitz <ori.markowitz@gmail.com>
2021-02-05 18:03:10 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  
+  const [sushi, setSushi] = useState([])
+  const [money, setMoney] = useState(100)
+  const [eatenSushis, setEatenSushis] = useState([])
+
+
+
+  const handleMoneySpent = (sushiPiece) => {
+    setMoney((money) => money - sushiPiece.price)
+    setEatenSushis([...eatenSushis, sushiPiece])
+
+  }
+
+  
+
+  useEffect(() => {
+    fetch(API)
+      .then(resp => resp.json())
+      .then(data => {
+        setSushi(data)
+      })
+  }, [])
+
+  console.log(sushi)
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushi={sushi}  handleMoneySpent={handleMoneySpent} money={money} />
+      <Table money={money} eatenSushis={eatenSushis} />
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-function Table({ plates = [] }) {
+function Table({ eatenSushis, money }) {
   // renders an empty plate for every element in the array
-  const emptyPlates = plates.map((_, index) => (
-    <div className="empty-plate" style={{ top: -7 * index }} />
+  const emptyPlates = eatenSushis.map((_, index) => (
+    <div key={index} className="empty-plate" style={{ top: -7 * index }} />
   ));
 
   return (
     <>
       <h1 className="remaining">
-        You have: ${/* Give me how much money I have left */} remaining!
+        You have: ${money} remaining!
       </h1>
       <div className="table">
         <div className="stack">{emptyPlates}</div>
